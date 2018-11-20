e8aa20e362cd2ac2de0e857f589c19aaa4207284
Sakuke <nvtrung1206@gmail.com>
2018-11-20 21:37:28 +0900

    show and turn back in swift

--- swoosh-app/swoosh-app/ViewController.swift
+++ swoosh-app/swoosh-app/ViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
+    @IBAction func unwindFromSkillVC(unwindSeuge: UIStoryboardSegue) {
+        
+    }
 
 }
 
